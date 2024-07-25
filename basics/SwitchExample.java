797e185568e34f7517ac9fbf94d2c70b0406dcbf
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-07-25 22:53:06 +0530

    Add files via upload

---
+++ basics/SwitchExample.java
@@ -0,0 +1,25 @@
+import java.util.List;
+public class SwitchExample {
+    static double switchCase(int choice, List<Double> arr){
+        // code here
+        switch(choice){
+            case 1:
+                double r=arr.get(0);
+                return Math.PI*r*r;
+                
+                
+            case 2:
+                double l=arr.get(0);
+                double b=arr.get(1);
+                return l*b;
+            
+                
+            default:
+            //System.out.println("invalid");
+            return -1;
+        }
+    
+    }
+}
+    
+
