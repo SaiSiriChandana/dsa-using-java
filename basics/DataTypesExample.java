c9fa3e885a6bd9a7ecdd6f080eded37abfe7929b
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-07-25 21:15:37 +0530

    Add files via upload

---
+++ basics/DataTypesExample.java
@@ -0,0 +1,13 @@
+class DatatypesExample {
+    static int dataTypeSize(String str) {
+        switch(str){
+            case "Integer":return 4;
+            case "Character":return 1;
+            case "Long":return 8;
+            case "Float":return 4;
+            case "Double":return 8;
+            default:return -1;
+        }
+    }
+
+}
