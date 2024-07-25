669747e0db1c278df503dd043b1338d2335030c2
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-07-25 19:53:57 +0530

    Add files via upload

---
+++ patterns/PatternTwentyOne.java
@@ -0,0 +1,20 @@
+public class PatternTwentyOne {
+    public static void pattern21(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||i==n-1||j==0||j==n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        pattern21(n);
+    }
+    
+}
