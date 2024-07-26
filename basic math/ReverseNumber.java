8f3288adeba058c0b36f174a6ae5320b1cc1cf3a
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-07-26 21:25:28 +0530

    Update ReverseNumber.java

--- basic math/ReverseNumber.java
+++ basic math/ReverseNumber.java
@@ -1,36 +1,22 @@
-class ReverseNumber {
-    public int reverse(int x) {
-        int revNum = 0;
+class ReverseNumber{
+    public int reverse(int x){
+        int revNum=0;
         int lastDigit;
-       
-
-        // Handle the special case for Integer.MIN_VALUE
-        if (x == Integer.MIN_VALUE) {
+        boolean isNegative=x<0;
+        x=Math.abs(x);
+        if (x==Integer.MIN_VALUE){
             return 0;
         }
-        boolean isNegative = x < 0;
-
-        x = Math.abs(x);
-
         while (x > 0) {
-            lastDigit = x % 10;
-            x = x / 10;
-            
-            // Check for overflow before updating revNum
-            if (revNum > (Integer.MAX_VALUE - lastDigit) / 10) {
-                return 0; // Overflow occurred
+            lastDigit=x%10;
+            x=x/10;
+           // Check for overflow before updating revNum
+            if (revNum>(Integer.MAX_VALUE-lastDigit)/10){
+                return 0;
             }
-
-            revNum = (revNum * 10) + lastDigit;
+            revNum=(revNum*10)+lastDigit;
         }
-
         return isNegative ? -revNum : revNum;
-    }
-
-    public static void main(String[] args) {
-        ReverseNumber solution = new ReverseNumber();
-        System.out.println(solution.reverse(-123)); // Should print -321
-        System.out.println(solution.reverse(2147483647)); // Should print 0 due to overflow
     }
 }
 
