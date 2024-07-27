e35033703ee434d4062df3303758ef7e122bf54f
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-07-27 17:00:07 +0530

    Add files via upload

---
+++ Recursion/PrintNNumbersWithoutLoop.java
@@ -0,0 +1,12 @@
+public class PrintNNumbersWithoutLoop {
+    int cnt=1;
+    public void printNos(int N)
+    {
+        //Your code here
+        if(cnt>N) return;
+        System.out.print(cnt+" ");
+        cnt++;
+        printNos(N);
+        
+    }
+}
