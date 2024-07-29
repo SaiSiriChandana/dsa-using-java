56f3aec1d2395e967433b2f6123b5f73d7138aa0
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-07-29 15:41:16 +0530

    Add files via upload

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,27 @@
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+        // code here such that selectionSort() sorts arr[]
+        //select method should return min element
+        int min=i;
+        for(int j=i;j<=arr.length-1;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        return min;
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    //code here
+	    //this method uses min from select method and sorts the arr
+	    for(int i=0;i<=n-2;i++){
+	        int mini=select(arr,i);
+	        int temp=arr[mini];
+	        arr[mini]=arr[i];
+	        arr[i]=temp;
+	    }
+	}
+}
