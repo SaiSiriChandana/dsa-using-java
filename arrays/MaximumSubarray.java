7b97513c5ec8363facf725d1c03433d4502a7b92
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-08-01 20:22:21 +0530

    Add files via upload

---
+++ arrays/MaximumSubarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum=nums[0];
+        int tempSum=0;
+        for(int i=0;i<nums.length;i++){
+            tempSum=tempSum+nums[i];
+            if(tempSum>maxSum){
+            maxSum=tempSum;
+        }
+        if(tempSum<0){
+            tempSum=0;
+        }
+        //if(tempSum>maxSum){
+          //  maxSum=tempSum;
+        //}
+        }
+         return maxSum;
+    }
+   
+}
