cfe9e53fcdaaf13cc754b064c7cadf4c5cfcf712
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-08-04 18:26:16 +0530

    Update LargestElement.java

--- arrays/LargestElement.java
+++ arrays/LargestElement.java
@@ -1,7 +1,7 @@
 class Solution {
     public static int largest(int n, int[] arr) {
         // code here
-        int max=0;
+        int max=arr[0];
         for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
                 max=arr[i];
