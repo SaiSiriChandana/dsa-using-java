0a85298e36d9f8da5774422911eb8554a34f9350
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-08-11 19:02:00 +0530

    Update MaximumSubarray.java

--- arrays/MaximumSubarray.java
+++ arrays/MaximumSubarray.java
@@ -1,3 +1,20 @@
+/* Kadane's Algorithm : Maximum Subarray Sum in an Array
+
+Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
+has the largest sum and returns its sum and prints the subarray.
+
+Examples
+Example 1:
+Input:
+ arr = [-2,1,-3,4,-1,2,1,-5,4] 
+
+Output:
+ 6 
+
+Explanation:
+ [4,-1,2,1] has the largest sum = 6. 
+*/
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum=nums[0];
