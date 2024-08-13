5c4fc52a84a873804ba50a2f578b474f6c48dfce
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-08-13 20:01:42 +0530

    Create Longest Consecutive Sequence.java

---
+++ arrays/Longest Consecutive Sequence.java
@@ -0,0 +1,86 @@
+/*Problem Statement: You are given an array of ‘N’ integers. You need to find the length of the longest sequence which contains the consecutive elements.
+  Example 1:
+Input:
+ [100, 200, 1, 3, 2, 4]
+
+Output:
+ 4
+
+Explanation:
+ The longest consecutive subsequence is 1, 2, 3, and 4.
+Brute-force Approach: (linear search)
+  Better Approach(Using sorting): 
+  Optimal Approach(Using Set data structure): 
+  
+
+
+import java.util.*;
+
+public class tUf {
+    public static int longestSuccessiveElements(int[] a) {
+        int n = a.length;
+        if (n == 0)
+            return 0;
+
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+
+        // put all the array elements into set
+        for (int i = 0; i < n; i++) {
+            set.add(a[i]);
+        }
+
+        // Find the longest sequence
+        for (int it : set) {
+            // if 'it' is a starting number
+            if (!set.contains(it - 1)) {
+                // find consecutive numbers
+                int cnt = 1;
+                int x = it;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = Math.max(longest, cnt);
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {100, 200, 1, 2, 3, 4};
+        int ans = longestSuccessiveElements(a);
+        System.out.println("The longest consecutive sequence is " + ans);
+    }
+}
+
+
+*/
+class Solution {
+    public int longestConsecutive(int[] nums) {
+     int n = nums.length;
+        if (n == 0) return 0;
+
+        //sort the array:
+        Arrays.sort(nums);
+        int lastSmaller = Integer.MIN_VALUE;
+        int cnt = 0;
+        int longest = 1;
+
+        //find longest sequence:
+        for (int i = 0; i < n; i++) {
+            if (nums[i] - 1 == lastSmaller) {
+                //a[i] is the next element of the
+                //current sequence.
+                cnt += 1;
+                lastSmaller = nums[i];
+            } else if (nums[i] != lastSmaller) {
+                cnt = 1;
+                lastSmaller = nums[i];
+            }
+            longest = Math.max(longest, cnt);
+        }
+        return longest;
+    }
+   
+}
