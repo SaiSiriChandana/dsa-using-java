d41b0382fc3be73f84d28fcf1f7d039456ba8ad9
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-08-25 21:48:36 +0530

    Update MoveZeroes.java

--- arrays/MoveZeroes.java
+++ arrays/MoveZeroes.java
@@ -1,26 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
-        int j=-1;
+        int posnonzero=0;
+        //non zero elements to the beginning of the array
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-       if(j==-1){
-         System.out.println(Arrays.toString(nums));
-         return;
-       } 
-        for(int i=j+1;i<n;i++){
             if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                j++;
-
+               nums[posnonzero]=nums[i];
+               posnonzero++;
             }
         }
-        System.out.println(Arrays.toString(nums));
+        //fill the rest of the array with zeroes
+        for(int i=posnonzero;i<n;i++){
+            nums[i]=0;
+        }
     }
 }
