6ba0bbc38359a9d0d8e9d7733bae1f2639a304d1
Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2024-08-25 21:42:36 +0530

    Update Rotate array by K elements.java

--- arrays/Rotate array by K elements.java
+++ arrays/Rotate array by K elements.java
@@ -67,22 +67,23 @@ public class Main {
     }
   }
   // Function to Rotate k elements to right
-  public static void Rotateeletoright(int[] arr, int n, int k) {
-    // Reverse first n-k elements
-    Reverse(arr, 0, n - k - 1);
-    // Reverse last k elements
-    Reverse(arr, n - k, n - 1);
-    // Reverse whole array
-    Reverse(arr, 0, n - 1);
-  }
-  public static void main(String args[]) {
-    int[] arr = {1,2,3,4,5,6,7};
-    int n = 7;
-    int k = 2;
-    Rotateeletoright(arr, n, k);
-    System.out.print("After Rotating the k elements to right ");
-    for (int i = 0; i < n; i++)
-      System.out.print(arr[i] + " ");
-    System.out.println();
-  }
+  class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; // In case k is greater than n
+        
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
